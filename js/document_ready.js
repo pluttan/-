9e4e30047043e5273249edc7170fde3cd1fe938b
Andrew Plutto <pluttan@ya.ru>
2021-12-16 19:56:13 +0300

    realise main

--- js/document_ready.js
+++ js/document_ready.js
@@ -4,53 +4,47 @@ var verticalCentered = false;
 $(document).ready(function () {
     let i = 0;
     function blurmain() {
-        $("#mainbg").css({ filter:"grayscale("+String(i / 200)+") blur(" + String(i / 10) + "px)"});
+        $("#mainbg").css({ filter: "grayscale(" + String(i / 200) + ") brightness(" + String(1 - i / 200) + ")" });
         i++;
     }
     function interval_of_blur() {
         mainbginterval = setInterval(blurmain, 10);
     }
-    setTimeout(() => interval_of_blur(), 3000);
-    setTimeout(() => clearInterval(mainbginterval), 4000);
+    setTimeout(() => interval_of_blur(), 2000);
+    setTimeout(() => clearInterval(mainbginterval), 3000);
 
     function mainh1_animation() {
         $("#mainh1").css({ display: "block" });
-        $("#mainh1").textillate({loop: false, in: { effect: "flipInY" } });
+        $("#mainh1").textillate({ loop: false, in: { effect: "flipInY" } });
     }
     function mainh2_animation() {
         $("#mainh2").css({ display: "block" });
-        $("#mainh2").textillate({loop: false, in: { effect: "flipInY",delayScale: 10000, sync: true} });
+        $("#mainh2").textillate({ loop: false, in: { effect: "flipInY", delayScale: 10000, sync: true } });
         $("#imgM").show(600);
     }
-    setTimeout(() => mainh1_animation(), 4000);
-    setTimeout(() => mainh2_animation(), 6000);
+    setTimeout(() => mainh1_animation(), 2000);
+    setTimeout(() => mainh2_animation(), 4000);
 
-    function bluricon(){
-        $("#icons").css({ filter:"blur(" + String(i*i*i) + "px)"});
-        i=i-0.1;
+    function bluricon() {
+        $("#icons").css({ filter: "blur(" + String(i * i * i) + "px)" });
+        i = i - 0.1;
     }
 
     function interval_of_icon() {
-        i=10.1;
+        i = 10.1;
         iconinterval = setInterval(bluricon, 10);
     }
 
     function end_interval_of_icon() {
         clearInterval(iconinterval);
-        $("#icons").css({ filter:"blur(0px)"});
+        $("#icons").css({ filter: "blur(0px)" });
     }
 
-    setTimeout(() => interval_of_icon(), 6000);
-    setTimeout(() => end_interval_of_icon(), 7000);
+    setTimeout(() => interval_of_icon(), 4000);
+    setTimeout(() => end_interval_of_icon(), 5000);
 
     $("#pagepiling").pagepiling({
         menu: "#menu",
-        anchors: ["main", "portfolio", "page3"],
-        navigation: {
-            textColor: "#f2f2f2",
-            bulletsColor: "#ccc",
-            position: "right",
-            tooltips: ["main", "portfolio", "Page 3", "Page 4"],
-        },
+        anchors: ["main", "portfolio", "about", "order", "comments"],
     });
 });
--- js/preloader.js
+++ js/preloader.js
@@ -8,39 +8,40 @@ function includeHTML() {
   /*loop through a collection of all HTML elements:*/
   z = document.getElementsByTagName("*");
   for (i = 0; i < z.length; i++) {
-      elmnt = z[i];
-      /*search for elements with a certain atrribute:*/
-      file = elmnt.getAttribute("include-html");
-      if (file) {
-          /*make an HTTP request using the attribute value as the file name:*/
-          xhttp = new XMLHttpRequest();
-          xhttp.onreadystatechange = function () {
-              if (this.readyState == 4) {
-                  if (this.status == 200) {
-                      elmnt.innerHTML = this.responseText;
-                  }
-                  if (this.status == 404) {
-                      elmnt.innerHTML = "Page not found.";
-                  }
-                  /*remove the attribute, and call this function once more:*/
-                  elmnt.removeAttribute("include-html");
-                  includeHTML();
-              }
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
           }
-          xhttp.open("GET", file, true);
-          xhttp.send();
-          /*exit the function:*/
-          return;
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
       }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
   }
 }
 includeHTML();
 
 $(window).on("load", function () {
-  $("#main").css({ "z-index": "-1" });
-  $("#mainbg").css({ "background-image":"url(img/imgsformainbg/" + String(getRandomInt(18)) + ".webp)",});
-  $("#imgM").hide(10);
 
+  $("#main").css({ "z-index": "-1" });
+  $("#mainbg").css({ "background-image": "url(img/imgsformainbg/" + String(getRandomInt(18)) + ".webp)", });
+  $("#imgM").hide(10);
+  entreg = false;
   $("#hat").show(200);
   $(".twobar").removeClass("cross").addClass("hamburger");
   $("#hat_head").css({
@@ -49,7 +50,7 @@ $(window).on("load", function () {
   });
   $("#hat").hide(200);
   function disp() {
-    $("#main_head").css({ "z-index": "1000000000" });
+    $("#hat_container").css({ "z-index": "1000000000" });
     $(".preloader").css({ display: "none" });
     $("#main").css({ "z-index": "10000" });
     $(".section").css({ display: "flex" });
