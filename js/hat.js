1b87d6bd40ecc041cc9a29f28a71cdbdeacb875a
droid123-123 <msjsnshu@gmail.com>
2021-12-17 14:11:56 +0300

    добавил блок о нас

--- js/hat.js
+++ js/hat.js
@@ -5,13 +5,30 @@ $(document).on("click", ".hamburger", function () {
     "border-top-left-radius": "0px",
   });
   $("#hat").show(200);
+  $("#hat").css({display:"inline"});
 });
 
 $(document).on("click", ".cross", function () {
+  if (entreg){
+    $('#black_bg').hide();
+    console.log("enter");
+    $('.enter_container').hide();
+    $('.register_container').hide();
+    $(".twobar").removeClass("cross").addClass("hamburger");
+    entreg=false;
+  }else{
   $(".twobar").removeClass("cross").addClass("hamburger");
   $("#hat_head").css({
     "border-bottom-left-radius": "10px",
     "border-top-left-radius": "10px",
   });
-  $("#hat").hide(200);
+  $("#hat").hide(200);}
 });
+
+function hide_onclick(){
+$(".twobar").removeClass("cross").addClass("hamburger");
+  $("#hat_head").css({
+    "border-bottom-left-radius": "10px",
+    "border-top-left-radius": "10px",
+  });
+  $("#hat").hide(200);};
