98ba14d4accbf0caac9c83f91396299b14ec41f5
Andrew Plutto <pluttan@ya.ru>
2021-12-20 17:32:12 +0300

    for img json

--- slider/swiper-master/scripts/build-angular.js
+++
@@ -1,9 +0,0 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-/* eslint no-console: "off" */
-const exec = require('exec-sh').promise;
-
-async function buildAngular() {
-  return exec(`ng build swiper --configuration production`);
-}
-
-module.exports = buildAngular;
