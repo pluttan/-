de7594da74ac7ea24626203723edeb173babb0e0
Andrew Plutto <pluttan@ya.ru>
2021-12-21 11:27:36 +0300

    картинки в портфолио

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,17 +3,30 @@ function getRandomInt(max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
 }
+
+function getimage() {
+    a = getRandomInt(4)
+    if (a == 1) { return { "background-image": "url(img/img_portfolio/nature_" + String(getRandomInt(100)) + ".jpg)" } }
+    else {
+        if (a == 2) { return { "background-image": "url(img/img_portfolio/city_" + String(getRandomInt(100)) + ".jpg)" } }
+        else {
+            if (a == 3) { return { "background-image": "url(img/img_portfolio/people_" + String(getRandomInt(100)) + ".jpg)" } }
+            else { if (a == 4) { return { "background-image": "url(img/img_portfolio/nature_" + String(getRandomInt(100)) + ".jpg)" } } }
+        }
+    }
+}
+
 $(document).ready(function () {
     lists = 3;
     photo = 6;
     for (list = 1; list < lists + 1; list++) {
         for (p = 1; p < photo + 2; p++) {
-            $("#portfolio_image_" + list + "_" + p).css({ "background-image": "url(img/nature/nature_" + String(getRandomInt(31)) + ".jpg)", });
+            $("#portfolio_image_" + list + "_" + p).css(getimage());
         }
     }
     i = 0;
     portfolio_animation = function () {
-        $("#portfolio_images").css({ "margin-left": -40-i });
+        $("#portfolio_images").css({ "margin-left": -40 - i });
         i++;
         console.log(i);
         if (i % 2000 == 0) {
@@ -31,5 +44,5 @@ $(document).ready(function () {
             setTimeout(() => get_bg(), 100)
         };
     };
-    
+
 });
